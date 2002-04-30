7f72069222c021918c6719adbd2bbc6354b470f5
Hazzl <Hazzl>
2002-04-30 13:02:50 +0000

    Changed e-mail address

--- greq/src/greq.c
+++ greq/src/greq.c
@@ -2,7 +2,7 @@
  * greq.c
  *
  * Copyright (C) 2001, Felix Braun, Berlin
- * EMail: fbraun@atdot.org
+ * EMail: Hazzl@developer.berlios.de
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
